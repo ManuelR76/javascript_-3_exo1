d9d86570e560060d6ed054c849709ee9a02e09d6
ManuelR76 <romeromanuel76@gmail.com>
2018-10-10 14:53:44 +0200

    exo1

---
+++ assets/JS/script.js
@@ -0,0 +1,5 @@
+var image1 = document.getElementById('image1');//récupération de la variable
+
+image1.addEventListener ("mouseover", function() {//création de la fonction
+    this.src = "images/image1_2.jpg"//récupération de la deuxième image
+})
